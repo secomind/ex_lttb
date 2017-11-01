780e3622e0e0babf0919b889fdf3652d47861570
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-02 00:12:49 +0100

    ex_lttb: implement trivial lttb cases

--- lib/ex_lttb.ex
+++ lib/ex_lttb.ex
@@ -2,4 +2,12 @@ defmodule ExLTTB do
   @moduledoc """
   Documentation for ExLTTB.
   """
+
+  def lttb(_sample_list, threshold) when threshold < 2 do
+    {:error, :invalid_threshold}
+  end
+
+  def lttb(sample_list, threshold) when threshold > length(sample_list) do
+    {:ok, sample_list}
+  end
 end
