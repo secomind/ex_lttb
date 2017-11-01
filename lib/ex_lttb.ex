2034a5b82b5caa0e99025bdfd6c181608ce985a3
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-02 00:09:18 +0100

    Remove hello world

--- lib/ex_lttb.ex
+++ lib/ex_lttb.ex
@@ -2,17 +2,4 @@ defmodule ExLTTB do
   @moduledoc """
   Documentation for ExLTTB.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExLTTB.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ex_lttb_test.exs
+++ test/ex_lttb_test.exs
@@ -1,8 +1,4 @@
 defmodule ExLTTBTest do
   use ExUnit.Case
   doctest ExLTTB
-
-  test "greets the world" do
-    assert ExLTTB.hello() == :world
-  end
 end
