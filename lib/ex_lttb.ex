74e2dcb59c24d2762ed7cc0fb9c2221851d8c7f9
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-11-02 02:53:15 +0100

    ex_lttb: fix make_buckets off-by-one
    Initialize with first and second samples and start from one and avg

--- lib/ex_lttb.ex
+++ lib/ex_lttb.ex
@@ -21,12 +21,13 @@ defmodule ExLTTB do
     Enum.map(sample_list, fn el -> [el] end)
   end
 
-  def make_buckets([head | tail] = sample_list, buckets_number) do
+  def make_buckets([first_sample, second_sample | tail] = sample_list, buckets_number) do
     # We subtract 2 since the first and last buckets are fixed,
     # containing the first and last sample
     avg = (length(sample_list) - 2) / (buckets_number -  2)
 
-    do_make_buckets(tail, 1, avg, avg, [[head]])
+    # The acc is populated from right to left and reversed at the end
+    do_make_buckets(tail, 1, avg, avg, [[second_sample], [first_sample]])
   end
 
   def average_point(points) do
@@ -38,11 +39,6 @@ defmodule ExLTTB do
 
   defp do_make_buckets([head | []], _current_index, _avg, _avg_acc, buckets_acc) do
     Enum.reverse([[head] | buckets_acc])
-  end
-
-  # Leave the first sample alone in its bucket
-  defp do_make_buckets([head | tail], current_index, avg, avg_acc, [_bucket_head | []] = buckets_acc) do
-    do_make_buckets(tail, current_index + 1, avg,  avg_acc + avg, [[head] | buckets_acc])
   end
 
   defp do_make_buckets([head | tail], current_index, avg, avg_acc, [bucket_head | bucket_tail] = buckets_acc) do
