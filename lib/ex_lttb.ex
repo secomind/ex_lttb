ccc0361ded9ea360b806a232166cdde255cb245e
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-03-06 11:28:33 +0100

    ex_lttb: use functions from PointUtils

--- lib/ex_lttb.ex
+++ lib/ex_lttb.ex
@@ -3,6 +3,8 @@ defmodule ExLTTB do
   Documentation for ExLTTB.
   """
 
+  alias ExLTTB.PointUtils
+
   def lttb(sample_list, threshold, opts \\ [])
 
   def lttb(_sample_list, threshold, _opts) when threshold < 2 do
@@ -51,44 +53,6 @@ defmodule ExLTTB do
     end
   end
 
-  defp get_x(point, opts) do
-    point_to_x_fun = Keyword.get(opts, :point_to_x_fun, fn point -> point[:x] end)
-
-    point_to_x_fun.(point)
-  end
-
-  defp get_y(point, opts) do
-    point_to_y_fun = Keyword.get(opts, :point_to_y_fun, fn point -> point[:y] end)
-
-    point_to_y_fun.(point)
-  end
-
-  defp xy_to_point(x, y, opts) do
-    xy_to_point_fun = Keyword.get(opts, :xy_to_point_fun, fn x, y -> %{x: x, y: y} end)
-
-    xy_to_point_fun.(x, y)
-  end
-
-  defp average_point(points, opts) do
-    {x_sum, y_sum} = Enum.reduce(points, {0, 0}, fn point, {x_sum, y_sum} -> {x_sum + get_x(point, opts), y_sum + get_y(point, opts)} end)
-    len = length(points)
-
-    xy_to_point(x_sum / len, y_sum / len, opts)
-  end
-
-  defp triangle_area(p1, p2, p3, opts) do
-    x1 = get_x(p1, opts)
-    y1 = get_y(p1, opts)
-
-    x2 = get_x(p2, opts)
-    y2 = get_y(p2, opts)
-
-    x3 = get_x(p3, opts)
-    y3 = get_y(p3, opts)
-
-    abs((x1 - x3) * (y2 - y1) - (x1 - x2) * (y3 - y1)) / 2
-  end
-
   defp select_samples([[first_sample] | tail] = _buckets, opts) do
     do_select_samples(tail, [first_sample], opts)
   end
@@ -98,14 +62,14 @@ defmodule ExLTTB do
   end
 
   defp do_select_samples([candidates, next_bucket | tail], [prev_point | _acc_tail] = acc, opts) do
-    next_point = average_point(next_bucket, opts)
+    next_point = PointUtils.average_point(next_bucket, opts)
 
     [initial_candidate | _tail] = candidates
-    initial_area = triangle_area(prev_point, initial_candidate, next_point, opts)
+    initial_area = PointUtils.triangle_area(prev_point, initial_candidate, next_point, opts)
 
     {selected_point, _area} =
       Enum.reduce(candidates, {initial_candidate, initial_area}, fn candidate_point, {best_point, best_area} ->
-        candidate_area = triangle_area(prev_point, candidate_point, next_point, opts)
+        candidate_area = PointUtils.triangle_area(prev_point, candidate_point, next_point, opts)
         if candidate_area > best_area do
           {candidate_point, candidate_area}
         else
