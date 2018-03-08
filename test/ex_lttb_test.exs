7f9ab0ebd208ca8fe0a6477d4a3824a3382a83d1
Riccardo Binetti <riccardo.binetti@ispirata.com>
2018-03-08 11:51:21 +0100

    ex_lttb_test: add property based testing

--- test/ex_lttb_test.exs
+++ test/ex_lttb_test.exs
@@ -1,4 +1,80 @@
 defmodule ExLTTBTest do
   use ExUnit.Case
+  use ExUnitProperties
   doctest ExLTTB
+
+  property "resulting list has always length <= threshold" do
+    greater_than_two_gen = gen all int <- positive_integer(), do: int + 2
+    ordered_sample_list_gen =
+      gen all sample_list <- list_of(fixed_map(%{x: float(), y: float()}), min_length: 2) do
+        Enum.sort(sample_list, fn %{x: xa}, %{x: xb} -> xa <= xb end)
+      end
+
+    check all sample_list <- ordered_sample_list_gen,
+              threshold <- greater_than_two_gen do
+      {:ok, result} = ExLTTB.lttb(sample_list, threshold)
+      assert length(result) == threshold || length(sample_list) <= threshold
+    end
+  end
+
+  property "the samples in the result are contained in the original list (including additional fields)" do
+    greater_than_two_gen = gen all int <- positive_integer(), do: int + 2
+    ordered_sample_list_gen =
+      gen all sample_list <- list_of(fixed_map(%{x: float(), y: float(), other: float()}), min_length: 2) do
+        Enum.sort(sample_list, fn %{x: xa}, %{x: xb} -> xa <= xb end)
+      end
+
+    check all sample_list <- ordered_sample_list_gen,
+              threshold <- greater_than_two_gen do
+      {:ok, result} = ExLTTB.lttb(sample_list, threshold)
+      assert Enum.all?(result, fn el -> Enum.member?(sample_list, el) end)
+    end
+  end
+
+  property "the first and last sample are the same of the original list" do
+    greater_than_two_gen = gen all int <- positive_integer(), do: int + 2
+    ordered_sample_list_gen =
+      gen all sample_list <- list_of(fixed_map(%{x: float(), y: float()}), min_length: 2) do
+        Enum.sort(sample_list, fn %{x: xa}, %{x: xb} -> xa <= xb end)
+      end
+
+    check all sample_list <- ordered_sample_list_gen,
+              threshold <- greater_than_two_gen do
+      {:ok, result} = ExLTTB.lttb(sample_list, threshold)
+      assert List.first(result) == List.first(sample_list)
+      assert List.last(result) == List.last(sample_list)
+    end
+  end
+
+  property "all properties hold also with data in a different shape with access functions" do
+    greater_than_two_gen = gen all int <- positive_integer(), do: int + 2
+    ordered_sample_list_gen =
+      gen all sample_list <- list_of(fixed_list([{:timestamp, float()}, {:data, float()}]), min_length: 2) do
+        Enum.sort(sample_list, fn a, b ->
+          xa = Keyword.fetch(a, :timestamp)
+          xb = Keyword.fetch(b, :timestamp)
+          xa <= xb
+        end)
+      end
+
+    sample_to_x_fun = fn sample -> Keyword.fetch!(sample, :timestamp) end
+    sample_to_y_fun = fn sample -> Keyword.fetch!(sample, :data) end
+    xy_to_sample_fun = fn x, y -> [timestamp: x, data: y] end
+
+    check all sample_list <- ordered_sample_list_gen,
+              threshold <- greater_than_two_gen do
+      {:ok, result} =
+        ExLTTB.lttb(
+          sample_list,
+          threshold,
+          sample_to_x_fun: sample_to_x_fun,
+          sample_to_y_fun: sample_to_y_fun,
+          xy_to_sample_fun: xy_to_sample_fun
+        )
+      assert length(result) == threshold || length(sample_list) <= threshold
+      assert Enum.all?(result, fn el -> Enum.member?(sample_list, el) end)
+      assert List.first(result) == List.first(sample_list)
+      assert List.last(result) == List.last(sample_list)
+    end
+  end
 end
